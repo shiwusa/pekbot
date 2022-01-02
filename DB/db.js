45ee057e8e0076fa5a9b04ca9ceca762b1e9dffc
shiwusa <lekh0t0er@gmail.com>
2022-01-02 23:46:55 +0200

    fixed connection to db

--- DB/db.js
+++ DB/db.js
@@ -1,8 +1,18 @@
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://shiwusa:shiwusa123@cluster0.vxkvc.mongodb.net/pekbot?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-    const collection = client.db("pekbot").collection("coursework");
-    console.log("connected to database");
-    client.close();
+const mongoose = require('mongoose');
+const Logger = require('../controllers/logger');
+
+mongoose.Promise = global.Promise;
+const DB_USER = "shiwusa";
+const DB_PASSWORD = "shiwusa123";
+
+//mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds054128.mlab.com:54128/pokerbot`, { useNewUrlParser: true });
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.vxkvc.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true });
+
+
+mongoose.connection.on('error', async (err) => {
+    Logger.add(`Connection error ${err}`);
+});
+
+mongoose.connection.on('open', () => {
+    Logger.add('Connect to MongoDB.');
 });
