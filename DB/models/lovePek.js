333b33804dce6e7ee3efa0b28b7253236ee9c756
shiwusa <lekh0t0er@gmail.com>
2022-01-07 01:44:09 +0200

    updated schemas

--- DB/models/lovePek.js
+++ DB/models/lovePek.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const LovePekSchema = new Schema({
     owner_id: { type: Schema.Types.ObjectId, ref: 'users' },
-    pek_name: {type: String, required: [true, "can't be blank"], index: true},
+    pek_name: {type: String, required: [true, "can't be blank"], index: true, unique: true},
     pek_species: {type: Boolean, required: [true, "can't be blank"], index: true}
 });
 
